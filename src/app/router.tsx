9d8c45438e5a4d3fa7da44ed5813e4323b2b92e9
PraddumnKumarShineDezign <praddumn.kumar@shinedezign.com>
2025-03-19 18:34:36 +0530

    solve bugs

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/pages/home';
 import AboutUs from './components/pages/aboutUs';
 import Services from './components/pages/services';
